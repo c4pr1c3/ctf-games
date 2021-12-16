9e329d92f02ce689730fa807d06a71dc65832bc0
c4pr1c3 <huangwei.me@cuc.edu.cn>
2021-12-16 13:57:27 +0800

    添加 vulfocus 的 docker-compose 编排封装

---
+++ fofapro/vulfocus/README.md
@@ -0,0 +1,43 @@
+## 纯净版 Kali 初始化基础环境
+
+```bash
+sudo apt update && sudo apt install -y docker.io docker-compose jq
+
+# 将当前用户添加到 docker 用户组，免 sudo 执行 docker 相关指令
+# 重新登录 shell 生效
+sudo usermod -a -G docker ${USER}
+
+# 切换到 root 用户
+sudo su -
+
+# 使用中科大 Docker Hub 镜像源
+cat <<EOF > /etc/docker/daemon.json
+heredoc> {
+  "registry-mirrors": ["https://docker.mirrors.ustc.edu.cn/"]
+}
+EOF
+
+# 重启 docker 守护进程
+systemctl restart docker
+
+# 提前拉取 vulfocus 镜像
+docker pull vulfocus/vulfocus:latest
+```
+
+## 快速上手
+
+对 [fofapro/vulfocus](https://github.com/fofapro/vulfocus) 提供傻瓜式二次封装，启动方式简化为
+
+1. `bash start.sh`
+2. 选择对外提供访问 `vulfocus-web` 的 IP
+3. 打开浏览器访问 admin / admin
+4. 【镜像管理】-【镜像管理】-【一键同步】
+5. 搜索感兴趣的漏洞镜像-【下载】
+6. 镜像下载完毕后，【首页】，随时可以【启动】镜像开始漏洞攻防实验了
+
+**Tested only on**
+
+- Kali Rolling 2021.2
+- Kali Rolling 2021.4
+
+
---
+++ fofapro/vulfocus/start.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+command -V jq 1>/dev/null 2>&1
+
+if [[ $? -ne 0 ]];then
+    echo "jq is required to run this script"
+    exit 1
+fi
+
+local_ips=()
+
+ifs=($(ls -l /sys/class/net/ | grep -v virtual | sed -n -E 's/.*net\/(.*)/\1/p'))
+for dev in "${ifs[@]}";do
+    if [[ -n "$dev" ]];then
+        ipv4_addr=$(ip -details -json a show "$dev" | jq -r '.[] | .addr_info[] | select(.family == "inet") | .local')
+	local_ips+=("$ipv4_addr")
+    fi
+done
+
+echo "========== physical ip list =========="
+echo "${local_ips[@]}"
+echo "Host-only IP is prefered in Virtualbox Setup"
+echo -n "plz enter your LOCAL_IP (default ${local_ips[0]}) "
+read LOCAL_IP
+
+isset_local_ip=0
+for ipv4_addr in "${local_ips[@]}";do
+    if [[ "$ipv4_addr" == "$LOCAL_IP" ]];then
+	isset_local_ip=1
+        break
+    fi
+done
+
+if [[ $isset_local_ip -eq 0 ]];then
+    LOCAL_IP="${local_ips[0]}"
+fi
+
+echo "$LOCAL_IP will be set as LOCAL_IP"
+
+cat <<EOF > vulfocus.env
+VUL_IP=${LOCAL_IP}
+EMAIL_HOST=${EMAIL_HOST}
+EMAIL_HOST_USER=${EMAIL_HOST_USER}
+EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}
+EOF
+
+docker-compose up -d
